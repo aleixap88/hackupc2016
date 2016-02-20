42c52cf484632d05a947a328f53f72d5b7eb99e9
Diego Lao Tebar <howarto@ymail.com>
2016-02-20 20:03:48 +0100

    Señores, el login de fb YA FUNCIONA

--- app/src/main/java/com/travelcoins/app/MainActivity.java
+++ app/src/main/java/com/travelcoins/app/MainActivity.java
@@ -2,7 +2,9 @@ package com.travelcoins.app;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
+import android.content.pm.PackageInstaller;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.os.Bundle;
@@ -15,11 +17,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -28,53 +35,81 @@ import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String PREFERENCES = "main_preferenc";
+    private static final String ID_USER = "id_user";
+    private static final String TOKEN_USER = "token_user";
+
+    private static final String LOGEADO = "logeado";
+
     private CallbackManager callbackManager;
     private TextView info;
     private LoginButton loginButton;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Saber si se ha hecho login previamente
+        final SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCES, 0);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        boolean login_previo = sharedPreferences.getBoolean(LOGEADO, false);
+
+
+        // -------------------------------------------
+
         super.onCreate(savedInstanceState);
 
         FacebookSdk.sdkInitialize(getApplicationContext());
+
+
+
         callbackManager = CallbackManager.Factory.create();
 
 
         setContentView(R.layout.activity_main);
 
+        //Para arreglar el tema del hash :
+        //Log.d("KeyHash:", printHashKey(getApplicationContext()));
 
-        //Para arreglar el tema del hash
-        Log.d("KeyHash:", printHashKey(getApplicationContext()));
-
-        info = (TextView)findViewById(R.id.info);
+        //info = (TextView)findViewById(R.id.info);
         loginButton = (LoginButton)findViewById(R.id.login_button);
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                info.setText("User ID:  " +
-                        loginResult.getAccessToken().getUserId() + "\n" +
-                        "Auth Token: " + loginResult.getAccessToken().getToken());
+
+                AccessToken accessToken = loginResult.getAccessToken();
+
+                Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(i);
+
+                //info.setText("User ID:  " +
+                //        loginResult.getAccessToken().getUserId() + "\n" +
+                //        "Auth Token: " + loginResult.getAccessToken().getToken());
             }
 
             @Override
             public void onCancel() {
-                info.setText("Login attempt cancelled.");
+                //info.setText("Login attempt cancelled.");
             }
 
             @Override
             public void onError(FacebookException e) {
-                info.setText("Login attempt failed.");
+                //info.setText("Login attempt failed.");
             }
         });
 
+        if (AccessToken.getCurrentAccessToken() != null) {
+            Intent i = new Intent(this, MapsActivity.class);
+            startActivity(i);
+        }
     }
 
     public void openmap(View view) {
         Intent i = new Intent(this, MapsActivity.class);
         startActivity(i);
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
