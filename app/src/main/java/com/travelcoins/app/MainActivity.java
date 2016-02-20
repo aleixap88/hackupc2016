f3c46b00631de59fc887ca5dfc155efb555fc96f
August <abozadev@gmail.com>
2016-02-20 11:19:17 +0100

    Added Map Activity

--- app/src/main/java/com/travelcoins/app/MainActivity.java
+++ app/src/main/java/com/travelcoins/app/MainActivity.java
@@ -1,38 +1,11 @@
 package com.travelcoins.app;
 
-import android.app.Activity;
-import android.content.Context;
-import android.location.Criteria;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-
-import br.com.condesales.EasyFoursquare;
-import br.com.condesales.EasyFoursquareAsync;
-import br.com.condesales.criterias.CheckInCriteria;
-import br.com.condesales.criterias.TipsCriteria;
-import br.com.condesales.criterias.VenuesCriteria;
-import br.com.condesales.listeners.AccessTokenRequestListener;
-import br.com.condesales.listeners.CheckInListener;
-import br.com.condesales.listeners.FoursquareVenuesRequestListener;
-import br.com.condesales.listeners.ImageRequestListener;
-import br.com.condesales.listeners.TipsRequestListener;
-import br.com.condesales.listeners.UserInfoRequestListener;
-import br.com.condesales.models.Checkin;
-import br.com.condesales.models.Tip;
-import br.com.condesales.models.User;
-import br.com.condesales.models.Venue;
-import br.com.condesales.tasks.users.UserImageRequest;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EasyFoursquareAsync async;
-    private EasyFoursquare sync;
-    private ArrayList<Venue> VenuesList;
-    private VenuesCriteria criteria;
-    private Context context;
 
 
     @Override
@@ -40,24 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        context = getApplicationContext();
-        async = new EasyFoursquareAsync(this);
-        //async.requestAccess(this);
+    }
 
-        async.getVenuesNearby(new FoursquareVenuesRequestListener() {
-            @Override
-            public void onVenuesFetched(ArrayList<Venue> venues) {
-                Venue v = venues.get(1);
-                String s = v.getId();
-                Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onError(String errorMsg) {
-
-            }
-        },criteria);
-
+    public void openmap(View view) {
+        Intent i = new Intent(this, MapsActivity.class);
+        startActivity(i);
     }
 
 
---
+++ app/src/main/java/com/travelcoins/app/MapsActivity.java
@@ -0,0 +1,78 @@
+package com.travelcoins.app;
+
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+import br.com.condesales.EasyFoursquareAsync;
+import br.com.condesales.criterias.VenuesCriteria;
+import br.com.condesales.listeners.FoursquareVenuesRequestListener;
+import br.com.condesales.models.Venue;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private EasyFoursquareAsync async;
+    private ArrayList<Venue> VenuesList;
+    private VenuesCriteria criteria;
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        context = getApplicationContext();
+        async = new EasyFoursquareAsync(this);
+        //async.requestAccess(this);
+
+        async.getVenuesNearby(new FoursquareVenuesRequestListener() {
+            @Override
+            public void onVenuesFetched(ArrayList<Venue> venues) {
+                Venue v = venues.get(1);
+                String s = v.getId();
+                Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(String errorMsg) {
+
+            }
+        },criteria);
+
+
+        // Add a marker in Sydney and move the camera
+        /*LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+    }
+}
