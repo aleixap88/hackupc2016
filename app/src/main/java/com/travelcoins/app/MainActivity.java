246c609419471598c77299d10c401b2d77ba11e9
Diego Lao Tebar <howarto@ymail.com>
2016-02-20 16:21:47 +0100

    Funcion debugeado hash fb

--- app/src/main/java/com/travelcoins/app/MainActivity.java
+++ app/src/main/java/com/travelcoins/app/MainActivity.java
@@ -1,8 +1,14 @@
 package com.travelcoins.app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 
 import android.app.Activity;
@@ -16,6 +22,9 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +42,10 @@ public class MainActivity extends AppCompatActivity {
 
 
         setContentView(R.layout.activity_main);
+
+
+        //Para arreglar el tema del hash
+        Log.d("KeyHash:", printHashKey(getApplicationContext()));
 
         info = (TextView)findViewById(R.id.info);
         loginButton = (LoginButton)findViewById(R.id.login_button);
@@ -66,6 +79,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+    public static String printHashKey(Context ctx) {
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                return Base64.encodeToString(md.digest(), Base64.DEFAULT);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            return "SHA-1 generation: the key count not be generated: NameNotFoundException thrown";
+        } catch (NoSuchAlgorithmException e) {
+            return "SHA-1 generation: the key count not be generated: NoSuchAlgorithmException thrown";
+        }
+
+        return "SHA-1 generation: epic failed";
     }
 
 }
